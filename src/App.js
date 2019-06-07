06e150b0971f270919fab23269b510fb7f95681c
Joaquim Barreto <joaquim@barreto.co>
2019-06-07 11:27:41 +0100

    styles.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="flex-container">
+        <nav class="nav-categories">
+          <h2>Categories</h2>
+
+          <ul class="nav-menu">
+            <li class="active">
+              <a href="#">Channels</a>
+            </li>
+            <li>
+              <a href="#">Dialer</a>
+            </li>
+            <li>
+              <a href="#">Optimization</a>
+            </li>
+            <li>
+              <a href="#">Reporting</a>
+            </li>
+            <li>
+              <a href="#">Voice Analytics</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
